251a3c0864c3edd14cffdd6e27a1e5052ba71cc0
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2021-04-24 15:18:19 +0200

    separated llvm ops

---
+++ src/main/oben/lang/llvm.clj
@@ -0,0 +1,51 @@
+(ns oben.lang.llvm
+  (:require [oben.lang.types :as t])
+  (:require [oben.lang.ast :as ast])
+  (:require [oben.lang.context :as ctx])
+  (:require [omkamra.llvm.ir :as ir]))
+
+(defmacro define-resize-op
+  [op]
+  `(defn ~op
+     [~'node ~'size]
+     (let [node-type# (t/type-of ~'node)
+           result-size# (ast/constant-value ~'size)
+           result-type# (t/resize node-type# result-size#)]
+       (ast/make-node result-type#
+         (fn [ctx#]
+           (let [ctx# (ctx/compile-node ctx# ~'node)
+                 ins# (~(symbol "omkamra.llvm.ir" (str op))
+                       (ctx/compiled ctx# ~'node)
+                       (t/compile result-type#)
+                       {})]
+             (ctx/compile-instruction ctx# ins#)))))))
+
+(define-resize-op trunc)
+(define-resize-op zext)
+(define-resize-op sext)
+(define-resize-op fptrunc)
+(define-resize-op fpext)
+
+(defmacro define-conversion-op
+  [op result-typeclass]
+  `(defn ~op
+     [~'node ~'size]
+     (let [node-type# (t/type-of ~'node)
+           result-size# (ast/constant-value ~'size)
+           result-type# (~result-typeclass result-size#)]
+       (ast/make-node result-type#
+         (fn [ctx#]
+           (let [ctx# (ctx/compile-node ctx# ~'node)
+                 ins# (~(symbol "omkamra.llvm.ir" (str op))
+                       (ctx/compiled ctx# ~'node)
+                       (t/compile result-type#)
+                       {})]
+             (ctx/compile-instruction ctx# ins#)))))))
+
+(define-conversion-op fptoui t/Int)
+(define-conversion-op fptosi t/SInt)
+(define-conversion-op uitofp t/FP)
+(define-conversion-op sitofp t/FP)
+;; (define-conversion-op ptrtoint)
+;; (define-conversion-op inttoptr)
+;; (define-conversion-op bitcast)
