97c4ba2baaac94e3418c023f42d7dc9f5bc23a42
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2021-11-02 08:20:52 +0100

    compiling LLVM modules and invoking native functions is the job of targets
    an Oben context should only care about compiling Oben code into LLVM IR
    
    targets take this LLVM IR, compile it into machine code (somewhere,
    maybe on another machine) and let the user invoke a compiled native
    function with given arguments (potentially sending arguments and
    receiving return values over some network)

--- src/main/oben/core/context.clj
+++ src/main/oben/core/context.clj
@@ -1,15 +1,6 @@
 (ns oben.core.context
-  (:require [clojure.core :as clj])
   (:require [oben.core.api :as o])
-  (:require [omkamra.llvm.ir :as ir])
-  (:require [omkamra.llvm.context :as llvm-context])
-  (:require [omkamra.llvm.buffer :as llvm-buffer])
-  (:require [omkamra.llvm.module :as llvm-module])
-  (:require [omkamra.llvm.engine :as llvm-engine])
-  (:import (java.nio ByteBuffer ByteOrder))
-  (:import (com.kenai.jffi Type CallContext CallingConvention
-                           Invoker HeapInvocationBuffer ArrayFlags))
-  (:require [midje.sweet :as m]))
+  (:require [omkamra.llvm.ir :as ir]))
 
 (def blockbin-ids [:entry :init :main :exit])
 
@@ -21,9 +12,7 @@
 
 (defn create
   []
-  {:llvm {:context nil
-          :ee nil}
-   :m (ir/module)
+  {:m (ir/module)
    :epoch 0
    :next-id 0
    :next-name nil
@@ -48,34 +37,6 @@
              :compiling-node nil
              :compiling-type nil)
       (update :epoch inc)))
-
-(defn get-llvm-context
-  [ctx]
-  (get-in ctx [:llvm :context]))
-
-(defn set-llvm-context
-  [ctx llvm-context]
-  (assoc-in ctx [:llvm :context] llvm-context))
-
-(defn dispose-llvm-context
-  [ctx]
-  (when-let [llvm-context (get-llvm-context ctx)]
-    (llvm-context/dispose llvm-context))
-  (update ctx :llvm dissoc :context))
-
-(defn get-llvm-execution-engine
-  [ctx]
-  (get-in ctx [:llvm :ee]))
-
-(defn set-llvm-execution-engine
-  [ctx ee]
-  (assoc-in ctx [:llvm :ee] ee))
-
-(defn dispose-llvm-execution-engine
-  [ctx]
-  (when-let [llvm-engine (get-llvm-execution-engine ctx)]
-    (llvm-engine/dispose llvm-engine))
-  (update ctx :llvm dissoc :ee))
 
 (defn assign-next-name
   [ctx prefix]
@@ -294,111 +255,3 @@
   [ctx type]
   (-> ctx
       (update :compiled-types dissoc type)))
-
-(defn assemble-module
-  [ctx]
-  (if (seq (:m ctx))
-    (let [module-src (ir/render-module (:m ctx))
-          buf (llvm-buffer/from-string module-src)
-          llvm-context (or (get-llvm-context ctx)
-                           (llvm-context/create))
-          mod (llvm-module/from-buffer buf llvm-context)
-          existing-ee (get-llvm-execution-engine ctx)
-          ee (or existing-ee
-                 (llvm-engine/create-mcjit-compiler mod))]
-      (when existing-ee
-        (llvm-engine/add-module existing-ee mod))
-      (-> ctx
-          (set-llvm-context llvm-context)
-          (set-llvm-execution-engine ee)))
-    ctx))
-
-(defn get-function-address
-  [ctx f]
-  (let [ee (get-llvm-execution-engine ctx)]
-    (llvm-engine/get-function-address ee (str (:name f)))))
-
-(defn jnr-type-of
-  [ir-type]
-  (case (ir/extract-type-tag ir-type)
-    :void Type/VOID
-    :float Type/FLOAT
-    :double Type/DOUBLE
-    :integer (let [[_ size] ir-type]
-               (case size
-                 1 Type/UINT8
-                 8 Type/UINT8
-                 16 Type/UINT16
-                 32 Type/UINT32
-                 64 Type/UINT64))
-    :ptr Type/POINTER
-    :array Type/POINTER))
-
-(defn invoker
-  [ctx f]
-  (let [address (get-function-address ctx f)
-        _ (when (zero? address)
-            (throw (ex-info "cannot get function address" {:function f})))
-        result-type (:result-type f)
-        param-types (map :type (:params f))
-        cc (CallContext/getCallContext
-            (jnr-type-of result-type)
-            (into-array Type (map jnr-type-of param-types))
-            CallingConvention/DEFAULT
-            false)
-        invoker (Invoker/getInstance)]
-    (fn [& args]
-      (let [hib (HeapInvocationBuffer. cc)]
-        (doseq [[arg type] (map vector args param-types)]
-          (case (ir/extract-type-tag type)
-            :integer (let [[_ size] type]
-                       (case size
-                         1 (.putByte hib (int arg))
-                         8 (.putByte hib (int arg))
-                         16 (.putShort hib (int arg))
-                         32 (.putInt hib (int arg))
-                         64 (.putLong hib (long arg))))
-            :float (.putFloat hib (float arg))
-            :double (.putDouble hib (double arg))
-            :ptr (letfn [(put-arg-as-array
-                           [arg-elt]
-                           (let [[_ object-type] type]
-                             (case (ir/extract-type-tag object-type)
-                               :array (let [[_ elt size] object-type]
-                                        (when (not= elt arg-elt)
-                                          (throw (ex-info "array element type mismatch"
-                                                          {:actual arg-elt :desired elt})))
-                                        (when (not= size (count arg))
-                                          (throw (ex-info "array size mismatch"
-                                                          {:actual (count arg) :desired size}))))
-                               (when (not= arg-elt object-type)
-                                 (throw (ex-info "array element type mismatch"
-                                                 {:actual arg-elt :desired object-type}))))
-                             (.putArray hib arg 0 (count arg) ArrayFlags/PINNED)))]
-                   (cond (integer? arg)
-                         (.putAddress hib (long arg))
-                         (instance? (Class/forName "[B") arg)
-                         (put-arg-as-array [:integer 8])
-                         (instance? (Class/forName "[S") arg)
-                         (put-arg-as-array [:integer 16])
-                         (instance? (Class/forName "[I") arg)
-                         (put-arg-as-array [:integer 32])
-                         (instance? (Class/forName "[J") arg)
-                         (put-arg-as-array [:integer 64])
-                         (instance? (Class/forName "[F") arg)
-                         (put-arg-as-array [:float 32])
-                         (instance? (Class/forName "[D") arg)
-                         (put-arg-as-array [:float 64])
-                         :else (throw (ex-info "cannot convert vector arg into array pointer" {:vector arg :type type}))))))
-        (case (ir/extract-type-tag result-type)
-          :void (do (.invokeInt invoker cc address hib) nil)
-          :ptr (.invokeAddress invoker cc address hib)
-          :integer (let [[_ size] result-type]
-                     (case size
-                       1 (unchecked-byte (.invokeInt invoker cc address hib))
-                       8 (unchecked-byte (.invokeInt invoker cc address hib))
-                       16 (unchecked-short (.invokeInt invoker cc address hib))
-                       32 (unchecked-int (.invokeInt invoker cc address hib))
-                       64 (unchecked-long (.invokeLong invoker cc address hib))))
-          :float (.invokeFloat invoker cc address hib)
-          :double (.invokeDouble invoker cc address hib))))))
