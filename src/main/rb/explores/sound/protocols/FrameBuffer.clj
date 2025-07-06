22a7145cbcb43b02a13afe78673ee68e0efece20
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2025-07-06 09:50:11 +0200

    removes sound explorations
    doing this stuff at such a high-level doesn't seem to make sense now

--- src/main/rb/explores/sound/protocols/FrameBuffer.clj
+++
@@ -1,7 +0,0 @@
-(ns rb.explores.sound.protocols.FrameBuffer)
-
-(defprotocol protocol
-  (nframes [_])
-  (nchannels [_])
-  (nsamples [_])
-  (->samples [_]))
--- src/main/rb/explores/sound/protocols/PhaseIndexed.clj
+++
@@ -1,4 +0,0 @@
-(ns rb.explores.sound.protocols.PhaseIndexed)
-
-(defprotocol protocol
-  (phase->value [_ phase]))
